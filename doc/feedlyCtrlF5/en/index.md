32ae69c7529beac2ba00b929fe29bd5877045ecb
spmbt <spmbt0@gmail.com>
2014-10-24 07:09:37 +0400

    habrajax  v.134

---
+++ doc/feedlyCtrlF5/en/index.md
@@ -0,0 +1,8 @@
+In the site feedly.com there are many keyboard shortcuts, for example, "R" - is "Refresh" of part of page, renewing of list of records. Unfortunately, there is bug in any national keyboard layout. It catch keyboard letter, but not key code. Script fix it and add such partial refresh by Ctrl-F5 or Shift-F5.
+
+Additions of  Oct, 20, 2014:
+
+* show date-time of page loading or partial refresh by "R").
+* author and title in floating block on top;
+* styles fixing
+* floating date+time (Oct,22)
--- userscript/feedlyCtrlF5/feedlyctrlf5.user.js
+++ userscript/feedlyCtrlF5/feedlyctrlf5.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @id          feedlyCtrlF5
-// @name        Feedly: partial refresh by "R" in any keyboard layout
-// @name:ru     Feedly: обновление списка по "R" в любом регистре
-// @version     2.2014.10.20
+// @name        Feedly: partial refresh by R in any keyboard layout
+// @name:ru     Feedly: обновление списка по R в любом регистре
+// @version     5.2014.10.22
 // @description Refresh by "R" in any national keyboard layout, add styles, date of refresh and floating title of opened article
 // @description:ru Обновляет по "R", исправляет стили, добавляет время обновления и висячий заголовок открытой статьи 
 // @namespace   github.com/spmbt
 // @include     http://feedly.com/*
-// @update 1 show date+time of load page or refresh; floating author+title of opened line
+// @update 4 show date+time of load page or refresh; floating author+title of opened line + float.date+time
 // ==/UserScript==
 
 //(en) In the site feedly.com there are many keyboard shortcuts, for example, "R" - is "Refresh" of part of page, renewing of list of records. Unfortunately, there is bug in any national keyboard layout. It catch keyboard letter, but not key code. Script fix it and add such partial refresh by Ctrl-F5 or Shift-F5.
@@ -23,7 +23,7 @@
 
 //Как бонус, добавляются стили, относящиеся к сайту, чтобы не добавлять их в юзер-стилях. Делают таблицу компактнее и убирают лишние кнопки (Твиттер и прочие). Отключить стили можно переменной CSS_ON в скрипте, установов в 0.
 
-(function(css){
+(function(userCss){
 	var CSS_ON =1; //user CSS enable
 
 var win = typeof unsafeWindow !='undefined'? unsafeWindow : (function(){return this})()
@@ -31,7 +31,6 @@ var win = typeof unsafeWindow !='undefined'? unsafeWindow : (function(){return t
 	,$q = function(q, el){return (el||document).querySelector(q)}
 	,ff
 	,Tout = function(h){
-		var th = this;
 		(function(){
 			if((h.dat = h.check(h.t) )) //wait of positive result, then occcurense
 				h.occur();
@@ -39,7 +38,7 @@ var win = typeof unsafeWindow !='undefined'? unsafeWindow : (function(){return t
 				win.setTimeout(arguments.callee, (h.t *= h.m) );
 		})();
 	}, tOuttime;
-new Tout({t:320, i:6, m: 1.6
+new Tout({t:320, i:7, m: 1.6
 	,check: function(){
 		return document && $q('#pageActionRefresh');
 	}
@@ -56,46 +55,59 @@ new Tout({t:320, i:6, m: 1.6
 		new Tout(tOuttime = {t:520, i:6, m: 1.6
 			,check: function(){
 				return $q('#section0 .label >div');
-			},occur: function(){
-				var NOW = new Date(), NOWmins = NOW.getMinutes();
-				this.dat.style.cssText ='text-align: right; font-size: 11px; font-style: italic; color: #a3a3a2';
-				this.dat.innerHTML = NOW.getFullYear() +'-'+ (NOW.getMonth() +1) +'-'+ NOW.getDate() +', '+ NOW.getHours() +':'+ (NOWmins>9?'':0) + NOWmins;
+			},occur: function($){
+				var NOW = new Date(), NOWmins = NOW.getMinutes()
+					,now = NOW.getFullYear() +'-'+ (NOW.getMonth() +1) +'-'+ NOW.getDate() +', '+ NOW.getHours() +':'+ (NOWmins>9?'':0) + NOWmins;
+				(!$ && this.dat || $).innerHTML = now;
+				$timeFloat && ($timeFloat.innerHTML = now);
 			}
 		});
 		document.addEventListener('keyup',ff);
 		var $flBar = $q('#floatingBar');
 		if($flBar){
-			var $lineTitleFloat = document.createElement('div');
+			var $lineTitleFloat = document.createElement('div')
+				,$timeFloat = document.createElement('div');
 			$lineTitleFloat.className ='lineTitleFloat';
 			$flBar.appendChild($lineTitleFloat);
 			$lineTitleFloat.innerHTML ='<span class=author title=""></span> <b class=title></b>';
+			$timeFloat.className ='timeFloat';
+			$flBar.insertBefore($timeFloat, $flBar.firstChild);
 		}
 		setInterval(function(){
 			var $inFrm = $q('.inlineFrame');
-			if($inFrm && $lineTitleFloat){
+			if(!$lineTitleFloat) return;
+			if($inFrm){
 				var author = $q('.metadata .sourceTitle', $inFrm)
 					,title = $q('.entryHeader .entryTitle', $inFrm);
-				$q('.author', $lineTitleFloat).innerHTML = author && author.innerHTML;
-				$q('.author', $lineTitleFloat).title = author && author.innerHTML;
-				$q('.title', $lineTitleFloat).innerHTML = title && title.innerHTML;
 			}
+			var $timeHead = $q('#section0 .label >div');
+			if($timeFloat && $timeHead && $timeHead.innerHTML =='')
+				tOuttime.occur($timeHead);
+			$q('.author', $lineTitleFloat).innerHTML = author && author.innerHTML ||'';
+			$q('.author', $lineTitleFloat).title = author && author.innerHTML ||'';
+			$q('.title', $lineTitleFloat).innerHTML = title && title.innerHTML ||'';
 		}, 999);
 	}
 });
-
-if(CSS_ON)
-	(function(css){ //addRules
-		if(typeof GM_addStyle !='undefined') GM_addStyle(css); //Fx,Chr
-		else if(typeof addStyle !='undefined') addStyle(css);
-		else{ //Op
-			var heads = document.getElementsByTagName('head');
-			if(heads.length){
-				var node = document.createElement('style');
-				node.type ='text/css';
-				node.appendChild(document.createTextNode(css));
-				heads[0].appendChild(node);
-		}}
-	})(css);
+(function(css){ //addRules
+	if(typeof GM_addStyle !='undefined') GM_addStyle(css); //Fx,Chr
+	else if(typeof addStyle !='undefined') addStyle(css);
+	else{ //Op
+		var heads = document.getElementsByTagName('head');
+		if(heads.length){
+			var node = document.createElement('style');
+			node.type ='text/css';
+			node.appendChild(document.createTextNode(css));
+			heads[0].appendChild(node);
+	}}
+})('#floatingBar,.floatingBar{padding-top:0!important; background: #f8f8f8}'
+  +'.pageActionBar{height: 18px!important}'
+  +'#floatingBar .pageactionbar{margin-right: 145px}'
+  +'#section0 .label >div{height: 1.3em!important; text-align: right; font-size: 11px; font-style: italic; color: #a3a3a2}'
+  +'.lineTitleFloat{height: 15px; line-height: 15px; margin: -15px -30px 0; padding-left: 3px; overflow: hidden; border-bottom: 2px solid #f8f8f8; background: #eee}'
+  +'.lineTitleFloat .author{display: inline-block; float: left; width: 127px; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;}'
+  +'.timeFloat{float: right; line-height: 18px; margin: 0 -28px 0 -18px; font-size: 11px; font-style: italic; color: #a3a3a2}'
+  + (CSS_ON? userCss :'') );
 
 })(
 ' #feedlyTabsHolder{width: 240px!important}'
@@ -113,10 +125,4 @@ if(CSS_ON)
   +'.u0Entry .condensedTools a:first-child{margin: 0 -5px 0 6px}'
   +'.u0Entry .lastModified{padding-left: 2px!important}'
   +'.u0EntryList{margin-top: 0!important}'
-  +'#floatingBar,.floatingBar{padding-top:0!important; background: #f8f8f8}'
-  +'.pageActionBar{height: 18px!important}'
-  +'#floatingBar .pageactionbar{padding-right: 180px}'
-  +'#section0 .label >div{height: 1.3em!important}'
-  +'.lineTitleFloat{height: 15px; line-height: 15px; margin: -15px -30px 0; padding-left: 3px; overflow: hidden; border-bottom: 2px solid #f8f8f8; background: #eee}'
-  +'.lineTitleFloat .author{display: inline-block; float: left; width: 127px; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;}'
 );
