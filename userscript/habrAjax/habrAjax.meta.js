368cb29e7529ae416ba3c36a10661462c631f669
spmbt <spmbt0@gmail.com>
2014-11-11 10:38:50 +0300

    HAjax 139, ZenC 4.21 - dates in similar posts; add namespaces

--- userscript/habrAjax/habrAjax.meta.js
+++ userscript/habrAjax/habrAjax.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id HabrAjax
 // @name HabrAjax
-// @version 137.2014.10.31
+// @version 138.2014.11.10
 // @namespace github.com/spmbt
 // @author spmbt0
 // @description Cumulative script with over 60 functions for Fx-Opera-Chrome
@@ -20,11 +20,9 @@
 // @exclude http://habrahabr.ru/api/*
 // @exclude http://habrahabr.ru/special/*
 // @exclude http://geektimes.ru/special/*
-// @update 136 namespaces;
-// @update 135 показ Geektimes в сайдбаре;
+// @update 136 namespaces; похожие посты; показ Geektimes в сайдбаре;
 // @update 134 namespaces; ссылка на ZenComment;
 // @update 133 восст. подсказки в настройках, перенос дублей из "Что обсуждают?" в "Лучшие", индикатора старых обсуждений; возврат опред_даты qa (toster.ru); стили в режиме без ZenComment (+без "специал"); дата загрузки стр.в меню;
-// @update 132 ,131 +geektimes; кнопки над полем ввода;
 // @resource meta habrAjax.meta.js
 // @icon data:image/gif;base64,R0lGODlhIAAgAMMBAG6Wyv///2+NtIucstfY2b/FzpSmvY+QkM3Nzunp6fLy8qGwweDg4MbFxa2trrm6uiwAAAAAIAAgAAAE/xDISau9OM/AOe2edoHBBwqiRZodmrKhRLqXYFfrdmLCQBQGWk62swgOiERAQQgChs9iRZBMKDgEFGnbMi4YDMU1gNBytzSJDcGwXhUD4lmqZofFioZrPqMIDARtYksIAzZ8dAINgngJVgkLUH1qBmBuCgmBYA6SUgKBl0wICA6lk1FdAAIFjngKDAgEpKYgWXIcKH8EDQ0EVwmjsrycIA4FZl2rDwcHDgivow8ODwzEHca3ASgDpMylsrEOzdUkDk59AtOl07wIDcwNkDbzCy7z8xIDD8Ps3Q5hCQqscxBHgw0DbEY1WIbEkRtHZV6oMsAq0wNqrcQ4KihR1Z9YjzUeKjjWcYqABUoaJeBY0k8bAm5ItqxgANjFBnBmTgnTQNw0nVOSNBjQLA1QXdEMATVioGnJCAA7
 // ==/UserScript==
